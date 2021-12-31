faeafc8b50a32599a86d2b060f7ec6265a1fe773
Poorna Chandra Bashyam <43184293+poornapunith@users.noreply.github.com>
2021-12-31 16:09:42 +0530

    Update settings.py

--- PicCollage/settings.py
+++ PicCollage/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -69,6 +70,7 @@ TEMPLATES = [
     },
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR,'ststicfiles')
 WSGI_APPLICATION = 'PicCollage.wsgi.application'
 
 
